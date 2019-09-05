7b65c3ee69bd55d534e71d9bb30c180f2dc4d178
Paul Thomason <pablo@pauls-macbook-pro.local>
2019-09-05 15:23:46 -0400

    Initial Commit

---
+++ areaOfRectangle/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  areaOfRectangle
+//
+//  Created by Paul Thomason on 9/5/19.
+//  Copyright © 2019 Paul Thomason. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
